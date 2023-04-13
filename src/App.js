a36e04305d00e182967d629e25930bcdfdcce712
HM Sadik Ahsan Mazumder <38665358+sadikmazumder@users.noreply.github.com>
2023-04-14 02:06:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UseEffectHooks from "./component/UseEffectHooks";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>useEffect Hooks</h1>
+      <UseEffectHooks />
+    </div>
+  );
+}
---
+++ src/component/UseEffectHooks.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+function UseEffectHooks() {
+  const [content, setContent] = useState();
+  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
+
+  // useEffect hooks - > using the lifeCycle methods
+
+  //Mount
+  useEffect(() => {
+    console.log("componentDidMount");
+  }, []);
+
+  //Update
+  useEffect(() => {
+    console.log("componentDidUpdate");
+
+    fetch("https://jsonplaceholder.typicode.com/" + content)
+      .then((response) => response.json())
+      //.then((json) => console.log(json));
+      .then((json) => setItems(json));
+  });
+
+  //UnMount
+  useEffect(() => {
+    console.log("componentDidUnmount");
+    return () => {
+      //cleaning
+      console.log("Unmount");
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
+      <button onClick={() => setContent("posts")}>Posts</button>
+      <button onClick={() => setContent("users")}> Users</button>
+      <button onClick={() => setContent("comments")}> Comments</button>
+
+      <h1>{content}</h1>
+
+      <ul>
+        {items &&
+          items.map((item) => {
+            // return <pre>{JSON.stringify(item)}</pre>;
+            return <li key={item.id}>{item.id}</li>;
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default UseEffectHooks;
